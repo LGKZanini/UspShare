671e487efa7ead2e35fb39e93c47ce64ef4d8fa6
Luiz Guilherme <luizkasputis@hotmail.com>
2020-10-09 23:38:36 -0300

    adicionando container, label e responsividade

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import { Header } from './components/header/header';
+import { Label } from './components/label/label'
 
 function App() {
   return (
     <>
       <Header />
+      <Label />
     </>
   );
 }

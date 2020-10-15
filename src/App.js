1c568579f3f153cd23916b91983a25c68d3e588e
Luiz Guilherme <luizkasputis@hotmail.com>
2020-10-15 16:43:47 -0300

    arrumando master

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { Header } from './components/header/header';
 import { BodyContainer } from './components/body/style';
 import { Label } from './components/label/label'
-import { Footer } from './components/footer/footer';
 import { SearchBar } from './components/search-box/search-box';
 
 function App() {
@@ -12,8 +11,7 @@ function App() {
       <Header inicio={10}/>
       <BodyContainer>
         <SearchBar />
-        <Label />
-        <Footer/>
+        <Label />=
       </BodyContainer>
     </>
   );

34631518a0f2e5e27036ae3705870deea84f2677
Luiz Guilherme <luizkasputis@hotmail.com>
2020-10-15 16:42:44 -0300

    add page home

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React from 'react';
 
-import { Header } from './components/header/header';
-import { BodyContainer } from './components/body/style';
-import { Label } from './components/label/label'
-import { Footer } from './components/footer/footer';
-import { SearchBar } from './components/search-box/search-box';
+import { Home } from './pages/home';
 
 function App() {
   return (
     <>
-      <Header inicio={10}/>
-      <BodyContainer>
-        <SearchBar />
-        <Label />
-        <Footer/>
-      </BodyContainer>
+      <Home />
     </>
   );
 }

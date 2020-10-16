e2643a2d0111237c5d0683b83fa6f9f4bf3149b9
Luiz Guilherme <luizkasputis@hotmail.com>
2020-10-16 18:35:41 -0300

    fix font

--- src/fonts/global-fonts.js
+++ src/fonts/global-fonts.js
@@ -7,15 +7,18 @@ import Quicksand2 from './quicksand-v2.woff2';
 
 export const CreateGlobalStyle = createGlobalStyle`
     @font-face {
-        font-family: 'Roboto Condensed';
-        src: local('Roboto Condensed'), local('Roboto Condensed'),
-        url(${RobotoCondensed2}) format('woff2'),
-        url(${RobotoCondensed}) format('woff');
-
         font-family: 'Quicksand';
         src: local('Quicksand'), local('Quicksand'),
         url(${Quicksand2}) format('woff2'),
         url(${Quicksand}) format('woff');
+        font-weight: 300;
+        font-style: normal;
+    }
+    @font-face {
+        font-family: 'Roboto Condensed';
+        src: local('Roboto Condensed'), local('Roboto Condensed'),
+        url(${RobotoCondensed2}) format('woff2'),
+        url(${RobotoCondensed}) format('woff');
         font-weight: 300;
         font-style: normal;
     }

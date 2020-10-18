124b33e8c0c78ef336b37b85dedf65d4a3484027
Luiz Guilherme <luizkasputis@hotmail.com>
2020-10-18 00:24:51 -0300

    add reponsive button

--- src/components/body/style.jsx
+++ src/components/body/style.jsx
@@ -8,6 +8,8 @@ export const BodyContainer = styled.body`
     flex-direction: column;
     justify-content: space-between;
 
-    background: url(${ () => BackGround });
+    background-image: url(${ () => BackGround });
     background-repeat: no-repeat;
+    box-shadow: inset  0px 10px 13px rgba(0, 0, 0, 0.2), 0px 20px 31px rgba(0, 0, 0, 0.14), 0px 8px 38px rgba(0, 0, 0, 0.12);;
+
 `;

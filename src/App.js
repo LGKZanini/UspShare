5a2f01fcd83d396a80e0115b0e89fb2e673ea70e
Luiz Guilherme <luizkasputis@hotmail.com>
2020-10-21 15:24:10 -0300

    fix rebase and gitignore package-lock.json

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
 
 import { CreateGlobalStyle } from './fonts/global-fonts';
-
-import { Header } from './components/header/header';
-import { BodyContainer } from './components/body/style';
-import { Label } from './components/label/label'
-import { SearchBar } from './components/search-box/search-box';
-import { Footer } from './components/footer/footer';
+import { Home } from './pages/home';
 
 function App() {
   return (
     <>
       <CreateGlobalStyle/>
-      <Header/>
-      <BodyContainer>
-        <SearchBar />
-        <Label />
-        <Footer />
-      </BodyContainer>
+      <Home />
     </>  
   );
 }

c4b9beb0f09e7cf6ccf021308c5f0f57056f0fe0
PietroSantana <pietrosantana@usp.br>
2020-10-22 10:32:55 -0300

    Pagina de Avaliacao de professores

---
+++ src/components/descricaoProfs/descricaoProfs.style.jsx
@@ -0,0 +1,57 @@
+import styled from 'styled-components';
+
+export const TextNomeProf = styled.a`
+    font-family: Quicksand;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 36px;
+    line-height: 40px;
+    margin: 0px;
+    color: #000000;
+`;
+
+export const TextTitle = styled.a`
+    font-family: Quicksand;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 28px;
+    line-height: 30px;
+
+    color: #000000;
+`;
+
+export const BodyDescription = styled.div`
+    margin-left: 150px;
+    color: #000000;
+`;
+
+export const NeckDescription = styled.div`
+    margin: 50px;
+    color: #000000;
+`;
+
+export const UserDescription = styled.div`
+    margin-left: 150px;
+    display: flex;
+    color: #000000;
+`;
+
+export const Borda = styled.img`
+    height: 150px;
+    width: 150px;
+    margin-top: 30px;
+    margin-left: 0px;
+    border-radius: 100%;
+    border: 5px solid #FDAF2D;
+`;
+
+export const TextDescricao = styled.a`
+    font-family: Quicksand;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 28px;
+    line-height: 30px;
+
+    color: #000000;
+`;
+
---
+++ src/components/header_log/style.jsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const TextUser = styled.a`
+    font-family: Quicksand;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 24px;
+    line-height: 30px;
+
+    color: #000000;
+`;
+
+
+export const TextCourse = styled.a`
+    font-family: Quicksand;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 14px;
+    line-height: 17px;
+    color: #000000;
+        
+`;

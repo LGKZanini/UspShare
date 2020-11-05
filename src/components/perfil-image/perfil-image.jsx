49e601726839d611b6d459912809c328b8197280
Luiz Guilherme <luizkasputis@hotmail.com>
2020-11-05 12:54:04 -0300

    add new test and fix some components

--- src/components/perfil-image/perfil-image.jsx
+++ src/components/perfil-image/perfil-image.jsx
@@ -11,7 +11,7 @@ export class PerfilImage extends React.Component {
     render() {
         return (
             <PerfilImageDiv size={this.props.size} auto={this.props.auto}>
-                <Image src="https://ocomecodafelicidade.com/wp-content/uploads/2018/12/como-parecer-uma-pessoa-bem-sucedida.jpg" />
+                <Image auto={this.props.auto} src="https://ocomecodafelicidade.com/wp-content/uploads/2018/12/como-parecer-uma-pessoa-bem-sucedida.jpg" />
                 <PerfilRating size={this.props.size} auto={this.props.auto}> {this.props.ranking}</PerfilRating>
             </PerfilImageDiv>
         );
